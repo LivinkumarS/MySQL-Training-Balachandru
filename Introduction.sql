ab6b2bb48ef5ce4499fcc275d2e7c6676d44cc95
LivinkumarS <001liv100@ggmail.com>
2025-08-13 13:51:06 +0530

    Introduction

---
+++ Introduction.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE employees_record;
+
+USE employees_record;
+
+CREATE TABLE employee_list(
+id int,
+employeeName varchar(100)
+);
+
+INSERT INTO employee_list (id, employeeName) VALUES (1,"Arya"),(2,"Surya"),(3,"Vijay"),(4,"Ajith"),(5,"Dhanush");
